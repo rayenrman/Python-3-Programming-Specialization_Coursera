c29d1d4238fed8cebb38e1f2f5615f4329fdfade
Raymond-Enriquez-Mandapat <raymenrimand@gmail.com>
2026-07-29 20:59:54 +0800

    Module 1/Turtle Graphics - Create topic_5.1.2.05_use_for_loop_to_draw_a_square.py

---
+++ Course 1 - Python Basics/Module 1/Turtle Graphics/topic_5.1.2.05_use_for_loop_to_draw_a_square.py
@@ -0,0 +1,13 @@
+import turtle
+wn = turtle.Screen()
+
+def close_window():
+    wn.bye()
+
+wn.listen()
+wn.onkey(close_window, "Escape")
+alex = turtle.Turtle()
+for i in range(4):
+    alex.forward(150)
+    alex.left(90)
+wn.mainloop()
