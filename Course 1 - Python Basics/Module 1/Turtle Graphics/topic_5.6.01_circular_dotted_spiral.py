a9f8d3f29552b4aec08f47dd8c1def79a892c37f
Raymond-Enriquez-Mandapat <raymenrimand@gmail.com>
2026-07-31 08:41:29 +0800

    Mod1/TurtleGrph - Modify topic_5.6.01_circular_dotted_spiral.py
    This pgm was modified as part of the ungraded app: 5.6. A Few More turtle Methods and Observations.
    It showed that only the last footprint of the shapes was created by stamp and this is the real tess.

--- Course 1 - Python Basics/Module 1/Turtle Graphics/topic_5.6.01_circular_dotted_spiral.py
+++ Course 1 - Python Basics/Module 1/Turtle Graphics/topic_5.6.01_circular_dotted_spiral.py
@@ -7,10 +7,29 @@ tess.shape("turtle")
 
 dist = 5
 tess.up()
-for _ in range(30):
+for i in range(30):
+    if i > 27:
+        tess.speed(1)
     tess.stamp()
     tess.forward(dist)
     tess.right(24)
     dist += 2
 
+tess.speed(1)
+tess.shape("classic")
+# tess.shape("arrow")
+# tess.shape("triangle")
+tess.color("magenta")
+tess.forward(150)
 wn.exitonclick()
+
+'''
+This program is part of the ungraded app 5.6. A Few More turtle Methods and Observations.
+
+All except one of the shapes you see on the screen here are footprints created by stamp. 
+But the program still only has one turtle instance — can you figure out which one is the real tess? 
+(Hint: if you’re not sure, write a new line of code after the for loop to change tess’ color, 
+or to put her pen down and draw a line, or to change her shape, etc.)
+
+Answer: The last turtle show on screen.
+'''
