64ce562de3186fa653a42803d9e2509100e828a1
Raymond-Enriquez-Mandapat <raymenrimand@gmail.com>
2026-08-04 18:56:43 +0800

    Mod3/p1 - Add item_12_vid_Automated_Tests_with_assert_1.py
    This program is in Module 3, Part 1 - Boolean Expressions,
         item 12 video, Automated_Tests_with_assert
    It shows the basic concept of the Python keywod 'assert'.
    Assert is a debugging keyword used to test if a specific condition in your code evaluates to True.
    If the condition is True, the program continues executing normally;
    if it evaluates to False, Python immediately halts the program and raises an AssertionError.

---
+++ Course 1 - Python Basics/Module 3 - Booleans and Conditionals/part 1 - Boolean Expressions/item_12_vid_Automated_Tests_with_assert_1.py
@@ -0,0 +1,5 @@
+print('Checking Assumpstions About Data Types')
+assert type(9//5) == int
+print('pgm executed: assert type(9//5) == int')
+assert type(9.0//5) == int
+print('pgm executed: assert type(9.0//5) == int')
